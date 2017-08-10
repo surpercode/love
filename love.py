8f63237337c4eecc9804507351b8aaed648e6a14
surpercode <30885964+surpercode@users.noreply.github.com>
2017-08-10 11:28:09 +0800

    Create love.py

---
+++ love.py
@@ -0,0 +1,143 @@
+# Douyaguang love Wangwenjing forever!
+# this script is done in 2017/8/10
+# I will run it in front of wenjing today
+# Give me good luck!!!
+
+
+mylove = '''
+
+
+               WWWWW                  WWWWW      EEEEEEEEEEEEEEEEEEE      NNNNNN          NNNNNN
+               WWWWW                 WWWWW       EEEEEEEEEEEEEEEEEEE      NNNNNNN         NNNNNN
+                WWWWW      WWWWW    WWWWW       EEEEE                    NNNNNNNNN       NNNNNN
+                WWWWW     WWWWWW   WWWWW        EEEEE                    NNNNNN NNN      NNNNNN
+                 WWWWW   WWW WWW  WWWWW        EEEEEEEEEEEEEEEEEEE      NNNNNN   NNN    NNNNNN
+                 WWWWW  WWW  WWW WWWWW         EEEEEEEEEEEEEEEEEEE      NNNNNN    NNN   NNNNNN
+                  WWWWWWWW   WWWWWWWW         EEEEE                    NNNNNN      NNN NNNNNN
+                  WWWWWWW    WWWWWWW          EEEEE                    NNNNNN       NNNNNNNNN
+                   WWWWW     WWWWWW          EEEEEEEEEEEEEEEEEEE      NNNNNN         NNNNNNN
+                   WWWWW     WWWWW           EEEEEEEEEEEEEEEEEEE      NNNNNN          NNNNNN
+
+
+                              JJJJJJ           IIIIII         NNNNNN          NNNNNN             GGGGGGGGG
+                              JJJJJJ           IIIIII         NNNNNNN         NNNNNN          GGGGGGGGGGGGGGGG
+                              JJJJJJ          IIIIII         NNNNNNNNN       NNNNNN        GGGGGG         GGGGGG
+                              JJJJJJ          IIIIII         NNNNNN NNN      NNNNNN       GGGGGG           GGGGGG
+                              JJJJJJ         IIIIII         NNNNNN   NNN    NNNNNN       GGGGGG
+               JJJJJJ         JJJJJJ         IIIIII         NNNNNN    NNN   NNNNNN       GGGGGG        GGGGGGGGG
+               JJJJJJ         JJJJJJ        IIIIII         NNNNNN      NNN NNNNNN         GGGGGG       GGGGGGGGG
+                JJJJJJ       JJJJJJ         IIIIII         NNNNNN       NNNNNNNNN          GGGGGG           GGGG
+                 JJJJJJJJJJJJJJJJ          IIIIII         NNNNNN         NNNNNNN            GGGGGGGGGGGGGGG GGGG
+                     JJJJJJJJJJ            IIIIII         NNNNNN          NNNNNN               GGGGGGGGGGGG GGGG
+
+                                                             IIIIII
+                                                             IIIIII
+                                                            IIIIII
+                                                            IIIIII
+                                                           IIIIII
+                                                           IIIIII
+                                                          IIIIII
+                                                          IIIIII
+                                                         IIIIII
+                                                         IIIIII
+
+
+                   LLLLLL                     OOOOOO         VVVVVV            VVVVVV    EEEEEEEEEEEEEEEEEEE
+                   LLLLLL                 OOOOOOOOOOOO       VVVVVV           VVVVVV     EEEEEEEEEEEEEEEEEEE
+                  LLLLLL               OOOOOOOOOOOOOOOOO      VVVVVV         VVVVVV     EEEEE
+                  LLLLLL             OOOOOO        OOOOOO     VVVVVV        VVVVVV      EEEEE
+                 LLLLLL             OOOOOO          OOOOOO     VVVVVV      VVVVVV      EEEEEEEEEEEEEEEEEEE
+                 LLLLLL             OOOOOO          OOOOOO     VVVVVV     VVVVVV       EEEEEEEEEEEEEEEEEEE
+                LLLLLL               OOOOOO        OOOOOO       VVVVVV   VVVVVV       EEEEE
+                LLLLLL                OOOOOOOOOOOOOOOOO         VVVVVV  VVVVVV        EEEEE
+               LLLLLLLLLLLLLLLLLLL      OOOOOOOOOOOO             VVVVVVVVVVVV        EEEEEEEEEEEEEEEEEEE
+               LLLLLLLLLLLLLLLLLLL        OOOOOO                 VVVVVVVVVVV         EEEEEEEEEEEEEEEEEEE
+
+
+                   YYYYYY         YYYYYY            OOOOOO              UUUUUU         UUUUUU
+                    YYYYYY      YYYYYY          OOOOOOOOOOOO           UUUUUU         UUUUUU
+                     YYYYYY   YYYYYY         OOOOOOOOOOOOOOOOO         UUUUUU         UUUUUU
+                      YYYYYYYYYYYY         OOOOOO        OOOOOO       UUUUUU         UUUUUU
+                       YYYYYYYYY          OOOOOO          OOOOOO     UUUUUU         UUUUUU
+                        YYYYYY            OOOOOO          OOOOOO     UUUUUU        UUUUUU
+                        YYYYYY             OOOOOO        OOOOOO      UUUUUU        UUUUUU
+                       YYYYYY               OOOOOOOOOOOOOOOOO         UUUUUU     UUUUUU
+                       YYYYYY                 OOOOOOOOOOOO             UUUUUUUUUUUUUU
+                      YYYYYY                    OOOOOO                   UUUUUUUU
+
+'''
+
+def lover_verify():
+        print '''
+         Are you wenjing?
+         A: yes      B: no
+        '''
+        i = 0
+        while(1):
+                ans = raw_input('>')
+                if ans == 'yes':
+                        ready()
+                        break
+                else:
+                        if i == 0:
+                                print 'Oh, no, I only love wenjing! I do not know who are you, you can leave now please!'
+                        if i == 1:
+                                print 'Oh, no, I realy only love wenjing! I do not know who are you, Do not bother me.'
+                        if i == 2:
+                                print 'This is the third time, I realy realy realy only love wenjing! Do i express it clear enough???'
+                        if i == 3:
+                                break
+                        i += 1
+
+def ready():
+        print '''
+         Are you ready?
+         A: yes      B: no
+        '''
+        while(1):
+                ans = raw_input('>')
+                if ans == 'yes':
+                        love_confirm()
+                        break
+                else:
+                        print 'invalid answer, please input again'
+
+def love_confirm():
+        print '''
+         Do you love Douyaguang?
+         A: yes      B: no
+        '''
+        i = 0
+        while(1):
+                ans = raw_input('>')
+                if ans == 'yes':
+                        print '''Oh, wenjing love me, yeah, yeah, yeah!!!
+I can not believe it, is it a dream?! Yeah, it is truth!!!
+I love you wenjing, I love you wenjing, I love you wenjing!!!
+The most import thing must be expressed three times!
+Wenjing, now is the magic time'''
+                        raw_input()
+                        show_love()
+                        break
+                else:
+                        if i == 0:
+                                print 'Douyaguang love you so much, why not love him. please input again'
+                        if i == 1:
+                                print 'No, Douyaguang will be very sad and the life will be meaningless if you do not love him? please inpput again'
+                        if i == 2:
+                                print 'I know you love him. Do not hesitate to fall in love with him! Go to input the yes, you will get a surprise!'
+                        if i == 3:
+                                print 'HumHumHum, Douyaguang love you forever whatever you do. No matter whether you choose the yes answer, I will show his love for you!!!!'
+                        if i == 4:
+                                print 'Now I must tell you the truth, I can only accept the yes answer, hahahahahaha! please input again'
+                        if i >= 5:
+                                print 'Please input yes!' + '!' * (i - 5)
+                        i += 1
+
+def show_love():
+        print ('\033[1;31;47m' + mylove)
+
+print '''This is a love travel when you start it, many intresting outputs will be a surprise to you!
+Now, GO!!!'''
+raw_input()
+lover_verify()
